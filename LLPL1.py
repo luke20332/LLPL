3f91b925e3f0a032e0a2213360b325b3083922f2
luke20332 <78817201+luke20332@users.noreply.github.com>
2022-08-30 16:02:34 +0100

    made version 2, with a different type of image processing applied. Despite being more in depth, it found results less than version 1

---
+++ LLPL1.py
@@ -0,0 +1,105 @@
+import cv2 as cv
+import numpy as np
+import os
+import time
+import pytesseract
+
+# where we installed pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\lukem\AppData\Local\Tesseract-OCR\tesseract.exe'
+
+
+img = cv.imread("sampleimages/1.jpg")  # small doesnt work
+
+#convert to greyscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+#blur image to remove noise
+
+blur = cv.GaussianBlur(gray, (0,0), sigmaX = 33, sigmaY = 33)
+
+#divide the np arrays on a per element basis
+
+divide = cv.divide(gray, blur, scale = 255)
+
+# Find the otsu threshold
+
+thresh = cv.threshold(divide, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+
+#cv.imshow("thresh", thresh)
+
+# Some morphological operations
+# Morphological operations are actions that can be performed on a binary image. One is called 'opening', and consists of erosion then dilation. Useful in removing noise
+
+# erosion
+# Simply put, erosion removes small pixels. It erodes the boundaries of the foreground object.
+# The kernel slides over the original image, as per convolution, and a pixel will be 1 if every pixel under the kernel (3,3) is also 1. This removes small pixels.
+# Thickness of foreground object decreases, and removes small white noises, and detatches 2 connected objects.
+
+# Dilation
+# The opposite of erosion, the kernel will make an element 1 if at least one of the elements underneath it is 1. Increases the white region in the image.
+
+kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
+
+morph = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+
+cv.imshow("morbed", morph)
+
+def rescaledImg(img):
+  # check which is bigger out of width and height
+  if img.shape[0] >= img.shape[1]:
+    scale = (720 / img.shape[0]) # ensure / not //, caused an error 
+  else:
+    scale = (720 / img.shape[1])
+    
+  width = int(img.shape[1] * scale) # find the scale to adjust w,h st the larger of the 2 is 720 pixels
+  height = int(img.shape[0] * scale)
+  dimensions = (width, height)
+
+  return cv.resize(img, dimensions, interpolation = cv.INTER_AREA) # return the resized image
+
+img = rescaledImg(img)
+
+
+
+contours, hierarchy = cv.findContours(morph, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+
+
+# create an empty text file to write to
+file = open("Interpreted text", "w+")
+file.write("")
+file.close()
+
+# then we loop through the contours list, passing it to pytesseract to identify any characters or blocks of text in the image. this is then written to the file.
+
+
+def findText():
+  for i in contours:
+    x, y, w, h = cv.boundingRect(i)
+
+    #overlay a rectangle
+    rect = cv.rectangle(morph, (x,y),(x+w, y+h), (0,0,255), 2)
+
+    cropped = morph[y:y+h, x:x+w]
+
+    cv.imshow("cropped",cropped)
+
+    file = open ("Interpreted text", "a")
+
+    text = pytesseract.image_to_string(cropped)
+    #text = "hello"
+
+    print(text)
+
+    file.write(text)
+    #file.write("\n")
+    print("searching...")
+    file.close 
+
+
+findText()
+
+print(len(contours))
+
+print("finished with search")
+cv.waitKey(0)
--- oldversions/LLPL.py
+++ oldversions/LLPL.py
@@ -8,7 +8,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\lukem\AppData\Local\Tesseract-OCR\tesseract.exe'
 
 
-img = cv.imread("sampleimages/8.jpg")  # small doesnt work
+img = cv.imread(r"C:\Users\lukem\Documents\projects\LLPL\sampleimages/6.jpg")  # small doesnt work
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 #cv.imshow("original", img)
 
@@ -39,6 +39,23 @@ threshold, img = cv.threshold(img, 100, 255, cv.THRESH_BINARY)
 
 # attempting to make a slideshow
 
+
+
+#for i in range (0,dir_size):
+  #path = os.path.join(DIR, i)
+"""
+for image in os.listdir(DIR):
+    img_path = os.path.join(DIR, image)
+    print (img_path)
+    img_array = cv.imread(img_path)
+"""
+ 
+    #img = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+    #img = rescaledImg(img)
+    #threshold, img = cv.threshold(img, 100, 255, cv.THRESH_BINARY)
+    #cv.imshow("image",img)
+    #time.sleep(3)
+   
 
 
 # Getting the boundary box
@@ -82,16 +99,18 @@ def findText():
 
     cropped = img2[y:y+h, x:x+w]
 
-    #cv.imshow("cropped",cropped)
+    #cv.imshow("cropped",cropped) # display when text found instead of always
 
     file = open ("Interpreted text", "a")
 
     text = pytesseract.image_to_string(cropped)
+
+    print(text)
     #text = "hello"
 
     file.write(text)
     #file.write("\n")
-    print("searching...")
+    #print("searching...")
     file.close 
 
 findText()
